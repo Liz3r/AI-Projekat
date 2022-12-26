54dbc69369840406237f71afba1de56ac95597b9
Guster <spavle.gh@gmail.com>
2022-12-26 11:47:50 +0100

    init

---
+++ main.py
@@ -0,0 +1,242 @@
+
+import re
+
+#m i n -> velicna table (n vrste m kolone)
+#na_potezu -> ko je na potezu (X ILI O)
+#xValue -> broj mogucnosti za smestanje plocice na treuntnoj tabli za igraca x
+#xoValue -> broj mogucnosti za smestanje plocice na treuntnoj tabli za igraca o
+# n - vrste
+# m - kolone
+
+def start_game():
+
+    global m,n,na_potezu,state
+    
+    pat = re.compile("[0-9][0-9]?")
+    inM = input("unesite broj vrsta: ")
+    inN = input("unesite broj kolona: ")
+    if(not pat.match(inM) or not pat.match(inN)):
+        print("unos nije validan (vrste i kolone)")
+        return False
+    m = int(inM)
+    n = int(inN)
+    na_potezu = 0
+
+    piPat = re.compile("0|1")
+    piIn = input("unesite igraca koji je prvi na potezu (0 - covek, 1 - racnar): ")
+    
+    if(not piPat.match(piIn)):
+        print("unos nije validan (prvi igra)")
+        return False
+    prvi_igra = int(piIn)
+
+    if(m < 5 or n < 5):
+        print("Velicina table ne validna, najmanja tabla je 5x5")
+        return False
+    if(prvi_igra != 0 and prvi_igra != 1):
+        print("unos nije validan")
+        return False
+    state = {
+        "xValue": (m-1)*n,
+        "oValue": (n-1)*m,
+        "matrica": [[None for b in range(n)] for c in range(m)],
+        "na_potezu": 0
+    }
+    return True
+
+def show_table(state):
+    global m,n
+    tabla_kolone = "  "
+    tabla_ivica = "  "
+    for i in range(0,m):
+        tabla_kolone += " " + chr(ord("A")+i)
+        tabla_ivica += " ="
+    print(tabla_kolone)
+    print(tabla_ivica)
+
+    kolNum = n
+    for row in reversed(state["matrica"]):
+        redStr = str(kolNum)+"||"
+        redIspod = "  "
+        for el in row:
+            if(el == None):
+                redStr += " |"
+                redIspod += " -"
+            if(el == "X"):
+                redStr += "X|"
+                redIspod += " -"
+            if(el == "O"):
+                redStr += "O|"
+                redIspod += " -"
+        redStr += "|" + str(kolNum)
+        print(redStr)
+        if(kolNum != 1):
+            print(redIspod)
+        kolNum -= 1
+
+    print(tabla_ivica)
+    print(tabla_kolone)
+
+def is_valid(state,vrsta,kolona):
+    global m,n
+    if(state["na_potezu"] == 0):
+        if(vrsta < n-1 and vrsta >= 0
+        and kolona < m and kolona >= 0
+        and state["matrica"][vrsta][kolona] == None 
+        and state["matrica"][vrsta+1][kolona] == None):
+            return True
+    else:
+        if(vrsta < n and vrsta >= 0
+        and kolona < m-1 and kolona >= 0
+        and state["matrica"][vrsta][kolona] == None 
+        and state["matrica"][vrsta][kolona+1] == None):
+            return True
+    return False
+
+def racunaj_XOVal(state):
+    stanje_provera = {
+    "xValue": state["xValue"],
+    "oValue": state["oValue"],
+    "matrica": state["matrica"].copy(),
+    "na_potezu": state["na_potezu"]
+    }
+    xVal = 0
+    oVal = 0
+    for row in range(0, n):
+        for col in range(0, m):
+            stanje_provera["na_potezu"] = 0
+            if(is_valid(stanje_provera,row,col)):
+                xVal += 1
+            stanje_provera["na_potezu"] = 1
+            if(is_valid(stanje_provera,row,col)):
+                oVal += 1
+    return (oVal,xVal)
+
+#odigrava potez
+#oblika (int)(char)
+#ne bavi se time ko mu je prosledio potez, samo proverava validnost
+#i postavlja potez na tablu ako je validan
+def igraj_potez(state,potez):
+    global m,n
+
+    pat = re.compile("[0-9][0-9]? [A-Z]")
+    if(not pat.match(potez)):
+        return False
+    p = potez.split(" ")
+    vrsta = int(p[0])-1
+    kolona = ord(p[1])-64-1
+
+    if(vrsta < 0 or vrsta > n or kolona < 0 or kolona > m):
+        return False
+    if(state["na_potezu"] == 0):
+        #provera validnosti poteza
+        if(is_valid(state,vrsta,kolona)):
+            state["matrica"][vrsta][kolona] = "X"
+            state["matrica"][vrsta+1][kolona] = "X"
+            #racunaj promenu xVal i oVal i zapamti u stanju
+            xo = racunaj_XOVal(state)
+            state["oValue"] = xo[0]
+            state["xValue"] = xo[1]
+            state["na_potezu"] = 1
+            return True
+        else:
+            return False
+    else:
+        #provera poteza
+        if(is_valid(state,vrsta,kolona)):
+            state["matrica"][vrsta][kolona] = "O"
+            state["matrica"][vrsta][kolona+1] = "O"
+            #racunaj promenu xVal i oVal i zapamti u stanju
+            xo = racunaj_XOVal(state)
+            state["oValue"] = xo[0]
+            state["xValue"] = xo[1]
+            state["na_potezu"] = 0
+            return True
+        else:
+            return False
+
+def game_in_progress():
+    global state
+    if(state["na_potezu"] == 0 and state["xValue"] == 0):
+        print("kraj igre\nigrac O je pobedio")
+        return False
+    if((state["na_potezu"] == 1 and state["oValue"] == 0)):
+        print("kraj igre\nigrac X je pobedio")
+        return False
+    return True
+
+def covek_protiv_coveka():
+    global state
+    while(not start_game()):
+        print("Unesite validne vrednosti")
+    show_table(state)
+
+    while(game_in_progress()):
+        while(not igraj_potez(state,input("unesite potez: "))):
+            print("potez nije validan, pokusajte ponovo: ")
+        show_table(state)
+        print(state["oValue"],state["xValue"])
+########################################
+
+def novo_stanje(state,vrsta,kolona):
+
+    novo_stanje_ret = {
+    "xValue": state["xValue"],
+    "oValue": state["oValue"],
+    "matrica": [x.copy() for x in state["matrica"]],
+    "na_potezu": state["na_potezu"]
+    }
+    if(vrsta < 0 or vrsta > n or kolona < 0 or kolona > m):
+        return False
+    if(novo_stanje_ret["na_potezu"] == 0):
+        #provera validnosti poteza
+        if(is_valid(novo_stanje_ret,vrsta,kolona)):
+            novo_stanje_ret["matrica"][vrsta][kolona] = "X"
+            novo_stanje_ret["matrica"][vrsta+1][kolona] = "X"
+            #racunaj promenu xVal i oVal i zapamti u stanju
+            xo = racunaj_XOVal(novo_stanje_ret)
+            novo_stanje_ret["oValue"] = xo[0]
+            novo_stanje_ret["xValue"] = xo[1]
+            novo_stanje_ret["na_potezu"] = 1
+            return novo_stanje_ret
+        else:
+            return False
+    else:
+        #provera poteza
+        if(is_valid(novo_stanje_ret,vrsta,kolona)):
+            novo_stanje_ret["matrica"][vrsta][kolona] = "O"
+            novo_stanje_ret["matrica"][vrsta][kolona+1] = "O"
+            #racunaj promenu xVal i oVal i zapamti u stanju
+            xo = racunaj_XOVal(novo_stanje_ret)
+            novo_stanje_ret["oValue"] = xo[0]
+            novo_stanje_ret["xValue"] = xo[1]
+            novo_stanje_ret["na_potezu"] = 0
+            return novo_stanje_ret
+        else:
+            return False
+
+def mogucnosti(state):
+    retLista = list()
+    for row in range(0, n):
+        for col in range(0, m):
+            ns = novo_stanje(state,row,col)
+            if(ns):
+                #show_table(ns)
+                retLista.append(ns)
+    return retLista
+
+covek_protiv_coveka()
+
+# start_game()
+# state["matrica"] = [
+#     ["X","X","X",None,None,None],
+#     ["X","X","X","O","O",None],
+#     ["X","X","X",None,"O","O"],
+#     ["X","X","X",None,None,None],
+#     ["X","X","X",None,"O","O"],
+#     ["X","X","X","O","O",None]
+# ]
+# show_table(state)
+# state["na_potezu"] = 0
+# for el in mogucnosti(state):
+#     show_table(el)
